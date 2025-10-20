3c92cd382f78a7bc032558bb9be682bbfb9b2f42
Sicong Hu <sinhco@outlook.com>
2025-10-20 15:45:06 +0800

    chore: 禁用2025中新增的 物理执行计划不等号走索引 补充date测试 (#39)
    Signed-off-by: SNC123 <sinhco@outlook.com>

--- src/observer/sql/optimizer/physical_plan_generator.cpp
+++ src/observer/sql/optimizer/physical_plan_generator.cpp
@@ -141,7 +141,7 @@ RC PhysicalPlanGenerator::create_plan(
     if (expr->type() == ExprType::COMPARISON) {
       auto comparison_expr = static_cast<ComparisonExpr *>(expr.get());
       // 简单处理，就找等值查询
-      if (comparison_expr->comp() != EQUAL_TO && comparison_expr->comp() != NOT_EQUAL) {
+      if (comparison_expr->comp() != EQUAL_TO /* && comparison_expr->comp() != NOT_EQUAL */) {
         continue;
       }
 
