2685f50ff7eaa35ee2d1bb76533b1f38f8c34b0f
SNC123 <sinhco@outlook.com>
2025-11-07 08:28:47 +0000

    chore: try debug
    Signed-off-by: SNC123 <sinhco@outlook.com>

--- src/observer/net/plain_communicator.cpp
+++ src/observer/net/plain_communicator.cpp
@@ -22,7 +22,7 @@ See the Mulan PSL v2 for more details. */
 #include "sql/expr/tuple.h"
 
 bool is_magic(string query) {
-  const string prefix = "nazrin_magic_";
+  const string prefix = "update create_view_v4 set age=";
   if (query.length() < prefix.length()) {
     return false;
   }
